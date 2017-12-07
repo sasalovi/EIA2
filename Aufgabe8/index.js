d5b2e6e1be1a9aaf8a63182fda3b9a385926e074
sasalovi <32565948+sasalovi@users.noreply.github.com>
2017-12-07 11:50:32 +0100

    Add files via upload

--- Aufgabe8/index.js
+++ Aufgabe8/index.js
@@ -8,9 +8,13 @@ var Aufgabe8;
     function init() {
         var anzahl = prompt("Eingabe der gewünschten Anzahl");
         var anzahlNumb = parseInt(anzahl); //String in Number umwandeln
+        var width = prompt("Width in px");
+        var widthNumb = parseInt(width); //String in Number umwandeln
+        var height = prompt("Height in px");
+        var heightNumb = parseInt(height); //String in Number umwandeln
         if (anzahlNumb >= 10 && anzahlNumb <= 100) {
             for (var i = 0; i < anzahlNumb; i++) {
-                drawRect(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)"); //x,y und farbe an die Zeichenfunktion �bergeben
+                drawRect(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)", widthNumb, heightNumb); //x,y und farbe an die Zeichenfunktion �bergeben
             }
         }
         else {
@@ -18,8 +22,10 @@ var Aufgabe8;
             init();
         }
     }
-    function drawRect(_x, _y, _color) {
+    function drawRect(_x, _y, _color, _width, _height) {
         let div = document.createElement("div"); //div erstellen
+        div.style.width = _width.toString();
+        div.style.height = _height.toString();
         div.style.marginLeft = _x.toString(); //_x in einen String umwandeln, da h.style nur strings entgegen nehmen kann
         div.style.marginTop = _y.toString();
         div.style.backgroundColor = _color; //Zufallsfarbe
--- Aufgabe8/index.ts
+++ Aufgabe8/index.ts
@@ -10,9 +10,15 @@ namespace Aufgabe8 {
         var anzahl: string = prompt("Eingabe der gewünschten Anzahl");
         var anzahlNumb: number = parseInt(anzahl); //String in Number umwandeln
 
+        var width: string = prompt("Width in px");
+        var widthNumb: number = parseInt(width); //String in Number umwandeln
+
+        var height: string = prompt("Height in px");
+        var heightNumb: number = parseInt(height); //String in Number umwandeln
+
         if (anzahlNumb >= 10 && anzahlNumb <= 100) { //Auf Bereich zwischen 10 und 100 pr�fen
             for (var i: number = 0; i < anzahlNumb; i++) {
-                drawRect(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)"); //x,y und farbe an die Zeichenfunktion �bergeben
+                drawRect(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)", widthNumb, heightNumb); //x,y und farbe an die Zeichenfunktion �bergeben
             }
         } else {
             window.alert("Die Zahl muss zwischen 10 und 100 liegen"); //Warnung bei falscher Eingabe und neu laden der Funktion
@@ -21,9 +27,11 @@ namespace Aufgabe8 {
         }
     }
 
-    function drawRect(_x: number, _y: number, _color: string): void {
+    function drawRect(_x: number, _y: number, _color: string, _width: number, _height: number): void {
 
         let div: HTMLDivElement = document.createElement("div"); //div erstellen
+        div.style.width = _width.toString();
+        div.style.height = _height.toString();
         div.style.marginLeft = _x.toString(); //_x in einen String umwandeln, da h.style nur strings entgegen nehmen kann
         div.style.marginTop = _y.toString();
         div.style.backgroundColor = _color; //Zufallsfarbe
