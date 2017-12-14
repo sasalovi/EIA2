c4b5dcbc43e9ef60cc2a6d9bb7e5ac6ea4fcf498
sasalovi <32565948+sasalovi@users.noreply.github.com>
2017-12-14 21:08:24 +0100

    Update index.ts

--- Aufgabe8/index.ts
+++ Aufgabe8/index.ts
@@ -11,14 +11,12 @@ namespace Aufgabe8 {
         var anzahlNumb: number = parseInt(anzahl); //String in Number umwandeln
 
         var width: string = prompt("Width in px");
-        var widthNumb: number = parseInt(width); //String in Number umwandeln
 
         var height: string = prompt("Height in px");
-        var heightNumb: number = parseInt(height); //String in Number umwandeln
 
         if (anzahlNumb >= 10 && anzahlNumb <= 100) { //Auf Bereich zwischen 10 und 100 pr�fen
             for (var i: number = 0; i < anzahlNumb; i++) {
-                drawRect(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)", widthNumb, heightNumb); //x,y und farbe an die Zeichenfunktion �bergeben
+                drawRect(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)", width, height); //x,y und farbe an die Zeichenfunktion �bergeben
             }
         } else {
             window.alert("Die Zahl muss zwischen 10 und 100 liegen"); //Warnung bei falscher Eingabe und neu laden der Funktion
@@ -27,14 +25,14 @@ namespace Aufgabe8 {
         }
     }
 
-    function drawRect(_x: number, _y: number, _color: string, _width: number, _height: number): void {
+    function drawRect(_x: number, _y: number, _color: string, _width: string, _height: string): void {
 
-        let div: HTMLDivElement = document.createElement("div"); //div erstellen
-        div.style.width = _width.toString();
-        div.style.height = _height.toString();
-        div.style.marginLeft = _x.toString(); //_x in einen String umwandeln, da h.style nur strings entgegen nehmen kann
-        div.style.marginTop = _y.toString();
+        let div: HTMLDivElement = document.createElement("div"); //div vom Typ HTMLDivElement erstellen
+        div.style.width = _width + "px";
+        div.style.height = _height + "px";
+        div.style.left = _x.toString() + "px"; //_x in einen String umwandeln, da h.style nur strings entgegen nehmen kann
+        div.style.top = _y.toString() + "px";
         div.style.backgroundColor = _color; //Zufallsfarbe
-        document.body.appendChild(div); //html ELement in den Code einf�gen lassen
+        document.body.appendChild(div); //html ELement in den Code einfügen lassen / anhängen
     }
 }
